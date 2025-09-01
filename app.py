d7b87f65fcdf9bd52e4cee48b92afc5b965fc5b1
Guilherme <guilhermej.santos1991@gmail.com>
2025-09-01 01:13:17 -0300

    Testando CSS

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ st.markdown("""
     :root {
         --primary-color: #147235;
         --secondary-color: #764ba2;
-        --background-color: #147235;
+        --background-color: #d8dae6;
         --card-bg: rgba(255, 255, 255, 0.95);
         --text-primary: #2d3748;
         --text-secondary: #718096;
